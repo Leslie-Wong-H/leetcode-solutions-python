e4fab24b623f674699420519e61139e4694412e7
Leslie Wong <79917148leslie@gmail.com>
2019-04-27 20:51:25 +0800

    Keypoint: judge the length   first

--- 28.implement-str-str.py
+++ 28.implement-str-str.py
@@ -54,5 +54,6 @@ class Solution:
         for i in range(len1 - len2 + 1):
             if haystack[i: i + len2] == needle:
                 return i
-        return -1
+        return - 1
+        
 
