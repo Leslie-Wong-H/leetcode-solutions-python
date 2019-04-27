c6a9824d4b8f4c5ecc72d72d697df9e59d2ee3c9
Leslie Wong <79917148leslie@gmail.com>
2019-04-27 17:29:05 +0800

    Rearrange the document tree

---
+++ 88.merge-sorted-array.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=88 lang=python3
+#
+# [88] Merge Sorted Array
+#
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# leetcode_solution_python
-My own python solutions to the leetcode problems. 
