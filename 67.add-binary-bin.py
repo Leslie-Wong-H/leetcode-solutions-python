7d76ef950f0f228bb23d8d65c23b60b80e5a380d
Leslie Wong <79917148leslie@gmail.com>
2019-04-27 21:30:46 +0800

    Keypoint: use python built-in function bin

---
+++ 67.add-binary-bin.py
@@ -0,0 +1,38 @@
+#
+# @lc app=leetcode id=67 lang=python3
+#
+# [67] Add Binary
+#
+# https://leetcode.com/problems/add-binary/description/
+#
+# algorithms
+# Easy (38.56%)
+# Total Accepted:    290.4K
+# Total Submissions: 752.9K
+# Testcase Example:  '"11"\n"1"'
+#
+# Given two binary strings, return their sum (also a binary string).
+#
+# The input strings are both non-empty and contains only characters 1 or 0.
+#
+# Example 1:
+#
+#
+# Input: a = "11", b = "1"
+# Output: "100"
+#
+# Example 2:
+#
+#
+# Input: a = "1010", b = "1011"
+# Output: "10101"
+#
+#
+
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+
+        return bin(int(a, 2) + int(b, 2))[2:]
+        
+        
