f2ff357aa21855958b48d9cdbf5304a2eb6e4235
Leslie Wong <79917148leslie@gmail.com>
2019-05-01 18:41:27 +0800

    Keypoint: append and assignment

--- 88.merge-sorted-array.py
+++ 88.merge-sorted-array.py
@@ -8,5 +8,11 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        for index in range(m):
+            nums2.append(nums1[index])
+        nums2 = sorted(nums2)
+        for index in range(m + n):
+            nums1[index] = nums2[index]
+            
         
 
