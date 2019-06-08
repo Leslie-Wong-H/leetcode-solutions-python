9b7cf369dc3f8df29a0632e81e1d67c1a217e414
Leslie Wong <79917148leslie@gmail.com>
2019-06-09 06:37:24 +0800

    Keypoint: BST, left small, right big, recursive

---
+++ 108.convert-sorted-array-to-binary-search-tree.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=108 lang=python3
+#
+# [108] Convert Sorted Array to Binary Search Tree
+#
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+
+        # recursively
+        
+        if not nums:
+            return None
+        
+        mid = len(nums) // 2
+
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid + 1 :])
+        
+        return root
+   
+        
+
+
+
