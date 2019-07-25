00a60b83be8f57da85d3a887baef2d5a734a8c36
Leslie Wong <79917148leslie@gmail.com>
2019-07-26 03:25:51 +0800

    Keypoint: border matters, take care of your neighbors

--- 289.game-of-life.py
+++ 289.game-of-life.py
@@ -22,41 +22,51 @@ class Solution:
             for j in range(boardColumn):
                 nAliveCnt = 0
 
+                # Check top left neighbor
                 if (i - 1 >= 0) and (j - 1 >= 0) and (board[i - 1][j - 1] == 1):
                     nAliveCnt += 1
                 
+                # Check top neighbor
                 if (i - 1 >= 0) and (board[i - 1][j] == 1):
                     nAliveCnt += 1
 
+                # Check top right neighbor
                 if (i - 1 >= 0) and (j + 1 < boardColumn) and (board[i - 1][j + 1] == 1):
                     nAliveCnt += 1
                 
+                # Check left neighbor 
                 if (j - 1 >= 0) and (board[i][j - 1] == 1):
                     nAliveCnt += 1
                 
+                # Check right neighbor 
                 if (j + 1 < boardColumn) and (board[i][j + 1] == 1):
                     nAliveCnt += 1
                 
+                # Check bottom left neighbor 
                 if (i + 1 < boardRow) and (j - 1 >= 0) and (board[i + 1][j - 1] == 1):
                     nAliveCnt += 1
                 
+                # Check bottom neighbor 
                 if (i + 1 < boardRow) and (board[i + 1][j] == 1):
                     nAliveCnt += 1
                 
+                # Check bottom right neighbor 
                 if (i + 1 < boardRow) and (j + 1 < boardColumn) and (board[i + 1][j + 1] == 1):
                     nAliveCnt += 1
 
-                
+                # Live
                 if board[i][j] == 1:
                     if (nAliveCnt == 2 or nAliveCnt == 3):
                         copyBoard[i][j] = 1
                     else:
                         copyBoard[i][j] = 0
                     
+                # Dead 
                 if board[i][j] == 0:
                     if nAliveCnt == 3:
                         copyBoard[i][j] = 1
 
+        # Recopy to board 
         for i in range(boardRow):
             for j in range(boardColumn):
                 board[i][j] = copyBoard[i][j]
