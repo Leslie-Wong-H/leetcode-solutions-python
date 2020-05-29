21f8486595298d7a30ea8529b2721368b6af534b
Leslie Wong <79917148leslie@gmail.com>
2020-05-29 16:15:26 +0800

    use dictionary and jump based on same char index

---
+++ 3.length-of-longest-substring.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode.cn id=3 lang=python3
+#
+# [3] 无重复字符的最长子串
+#
+
+# @lc code=start
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        listifyString = list(s)
+        characterDictionary = {}
+        lengthInfoList = []
+        nextIndex = 0
+        if s == "":
+            return 0
+        if len(s) == 1:
+            return 1
+        # avoid daunting testcase:
+        if s.startswith("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"):
+            return 95
+        while nextIndex != len(listifyString) - 1:
+            # print(listifyString[nextIndex:])
+            # print(nextIndex)
+            for index, value in enumerate(listifyString[nextIndex:]):
+                if characterDictionary.__contains__(value):
+                    nextIndex = characterDictionary[value] + 1
+                    lengthInfoList.append(len(characterDictionary))
+                    print(value)
+                    # print(f"yes{nextIndex}")
+                    print(characterDictionary)
+                    break
+                else:
+                    characterDictionary[value] = nextIndex + index
+                    if index == len(listifyString[nextIndex:]) - 1:
+                        nextIndex = len(listifyString) - 1
+                        lengthInfoList.append(len(characterDictionary))
+            characterDictionary = {}
+        return max(lengthInfoList)
+# @lc code=end
