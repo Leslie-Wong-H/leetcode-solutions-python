ce4fe197dbd3f971135a4900a006e2070a59ed7f
Leslie Wong <79917148leslie@gmail.com>
2020-06-08 13:31:36 +0800

    DFS, Iteration, and Regression

---
+++ 111.min-depth.py
@@ -0,0 +1,50 @@
+#
+# @lc app=leetcode.cn id=111 lang=python3
+#
+# [111] 二叉树的最小深度
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        # if not root:
+        #     return 0
+
+        # children = [root.left, root.right]
+        # # if we're at leef node
+        # if not any(children):
+        #     return 1
+
+        # min_depth = float('inf')
+        # for c in children:
+        #     if c:
+        #         min_depth = min(self.minDepth(c), min_depth)
+        # return min_depth + 1
+
+        ###
+        
+        if not root:
+            return 0
+        else:
+            stack, min_depth = [(1, root), ], float('inf')
+
+        while stack:
+            depth, root = stack.pop()
+            children = [root.left, root.right]
+            if not any(children):
+                min_depth = min(depth, min_depth)
+
+            for c in children:
+                if c:
+                    stack.append((depth + 1, c))
+
+        return min_depth
+# @lc code=end
